a4474dae7a0edaf172897ea18fca966777e0a41a
Tayyba-2006 <tayybahasnain2006@gmail.com>
2026-02-21 22:02:12 +0500

    Add files via upload

---
+++ cow.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ system("color 87");
+cout<<" -----------------------------------    "<<endl;
+cout<<" \n";
+cout<<"        o  ^__^                         "<<endl;
+cout<<"         o (oo)\\__________             "<<endl;
+cout<<"           (__)\\          )\\/\\       "<<endl;
+cout<<"               ||--------w |            "<<endl;
+cout<<"               ||         ||            "<<endl;
+}             
+
+
+
