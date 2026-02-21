d29a3974ccc12c2bcaa21f2448a40f8d8e251243
Tayyba-2006 <tayybahasnain2006@gmail.com>
2026-02-22 00:23:29 +0500

    Add files via upload

---
+++ gun.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+  system("color 09");
+cout<<"              +--^----------,--------,-----,--------^-,          "<<endl;
+cout<<"               | |||||||||   '--------'     |          O         "<<endl;
+cout<<"               '+---------------------------^----------|         "<<endl;
+cout<<"                 ',---------,---------,--------------'           "<<endl;
+cout<<"                   / XXXXXX /'|       /'                         "<<endl;
+cout<<"                  / XXXXXX /  |      /'                          "<<endl;
+cout<<"                 / XXXXXX /'-------'                             "<<endl;
+cout<<"                / XXXXXX /                                       "<<endl;
+cout<<"               / XXXXXX /                                        "<<endl;
+cout<<"              (________(                                         "<<endl;
+cout<<"               '______'                                          "<<endl;
+}  
