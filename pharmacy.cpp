02804e604d8b9677bbc58fc9e7119260cc5ac64c
Tayyba-2006 <tayybahasnain2006@gmail.com>
2026-02-22 16:04:11 +0500

    Add files via upload

---
+++ pharmacy.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; 
+main() 
+{
+   system("color 85");
+    cout<<"\n";
+    cout<<" ====================================================================== "<<endl;
+    cout<<" *                                                                    * "<<endl;
+    cout<<" *                 HEALTH-CARE PHARMACY MANAGEMENT SYSTEM             * "<<endl;
+    cout<<" *                                                                    * "<<endl;
+    cout<<" ====================================================================== "<<endl;
+}
